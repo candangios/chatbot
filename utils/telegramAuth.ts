e7f0641687be10a601aafea92cc2fba94b023bbb
candangios <candd1707@gmail.com>
2024-10-04 15:39:42 +0700

    Update telegramAuth.ts

--- utils/telegramAuth.ts
+++ utils/telegramAuth.ts
@@ -47,10 +47,10 @@ export function validateTelegramWebAppData(telegramInitData: string): Validation
   // }
   const dataCheckString = Array.from(initData.entries())
     .sort(([a], [b]) => a.localeCompare(b))
-    .map(([key, value]) => `${key} = ${value}`)
+    .map(([key, value]) => `${key}=${value}`)
     .join('\n')
 
-  const secretKey = crypto.createHmac('sha256', 'WebData').update(BOT_TOKEN!).digest()
+  const secretKey = crypto.createHmac('sha256', 'WebAppData').update(BOT_TOKEN).digest()
   const caculatedHash = crypto.createHmac('sha256', secretKey).update(dataCheckString).digest('hex')
 
   if (caculatedHash === hash) {
