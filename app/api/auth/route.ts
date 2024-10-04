61789d819d8f091d9e68b162b69f6d16f9a67ccf
candangios <candd1707@gmail.com>
2024-10-04 16:06:28 +0700

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -13,11 +13,11 @@ export async function POST(request: NextRequest) {
 
     // create new session
 
-    // const expires = new Date(Date.now() + SESSTION_DURATION)
-    // const session = await encrypt({ user, expires })
+    const expires = new Date(Date.now() + SESSTION_DURATION)
+    const session = await encrypt({ user, expires })
 
-    // // save the sesstion in a cookie
-    // cookies().set('session', session, { expires, httpOnly: true })
+    // save the sesstion in a cookie
+    cookies().set('session', session, { expires, httpOnly: true })
     return NextResponse.json({ message: 'Authentication successful' });
 
   } else {
