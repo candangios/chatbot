87510635b0d69b9b42e93145f7ccc3c4668d30a4
candangios <candd1707@gmail.com>
2024-10-11 10:35:58 +0700

    update

--- app/mission/page.tsx
+++ app/mission/page.tsx
@@ -2,7 +2,6 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
 import { AI } from '@/lib/chat/actions'
-import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
 import { Button } from '@/components/ui/button'
