9e276ed8cdaaf601be1825429e9412027441283a
candangios <candd1707@gmail.com>
2024-10-11 10:47:11 +0700

    update

--- components/sidebar-list.tsx
+++ components/sidebar-list.tsx
@@ -16,28 +16,29 @@ const loadChats = cache(async (userId?: string) => {
 
 export async function SidebarList({ userId }: SidebarListProps) {
   const chats = await loadChats(userId)
+  return null
 
-  if (!chats || 'error' in chats) {
-    redirect('/')
-  } else {
-    return (
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <div className="flex-1 overflow-auto">
-          {chats?.length ? (
-            <div className="space-y-2 px-2">
-              <SidebarItems chats={chats} />
-            </div>
-          ) : (
-            <div className="p-8 text-center">
-              <p className="text-sm text-muted-foreground">No chat history</p>
-            </div>
-          )}
-        </div>
-        <div className="flex items-center justify-between p-4">
-          <ThemeToggle />
-          <ClearHistory clearChats={clearChats} isEnabled={chats?.length > 0} />
-        </div>
-      </div>
-    )
-  }
+  // if (!chats || 'error' in chats) {
+  //   redirect('/')
+  // } else {
+  //   return (
+  //     <div className="flex flex-1 flex-col overflow-hidden">
+  //       <div className="flex-1 overflow-auto">
+  //         {chats?.length ? (
+  //           <div className="space-y-2 px-2">
+  //             <SidebarItems chats={chats} />
+  //           </div>
+  //         ) : (
+  //           <div className="p-8 text-center">
+  //             <p className="text-sm text-muted-foreground">No chat history</p>
+  //           </div>
+  //         )}
+  //       </div>
+  //       <div className="flex items-center justify-between p-4">
+  //         <ThemeToggle />
+  //         <ClearHistory clearChats={clearChats} isEnabled={chats?.length > 0} />
+  //       </div>
+  //     </div>
+  //   )
+  // }
 }
